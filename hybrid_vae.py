39bd27ddca0900d42feba79cfa14ad518cb93261
zwfightzw <zhouwei14@nudt.edu.cn>
2020-05-09 15:12:51 +0800

    zw

--- hybrid_vae.py
+++ hybrid_vae.py
@@ -51,7 +51,7 @@ class FullQDisentangledVAE(nn.Module):
         #self.z_prior_out_list = [nn.Linear(self.hidden_dim//self.block_size, self.z_dim * 2//self.block_size).to(device) for i in range(self.block_size)]
         self.z_prior_out_list = nn.Linear(self.hidden_dim, self.z_dim * 2).to(device)
 
-        self.z_to_c_fwd_list = [GRUCell(input_size=self.z_dim, hidden_size=self.hidden_dim).to(self.device)
+        self.z_to_c_fwd_list = [GRUCell(input_size=self.z_dim, hidden_size=self.hidden_dim//self.block_size).to(self.device)
             for i in range(self.block_size)]
 
         self.z_w_function = nn.Linear(self.hidden_dim, self.block_size) #nn.Sequential(nn.Linear(self.hidden_dim, self.hidden_dim//2),nn.ReLU(), nn.Linear(self.hidden_dim//2, self.block_size))
@@ -89,8 +89,8 @@ class FullQDisentangledVAE(nn.Module):
         post_z_1 = self.reparameterize(zt_1_mean, zt_1_lar, self.training)
         # init wt
         wt = torch.ones(batch_size, self.block_size).to(self.device)
-        z_fwd_list = [torch.zeros(batch_size, self.hidden_dim).to(self.device) for i in range(self.block_size)]
-        z_fwd_all = torch.stack(z_fwd_list, dim=2).mean(dim=2).view(batch_size, self.hidden_dim)
+        z_fwd_list = [torch.zeros(batch_size, self.hidden_dim//self.block_size).to(self.device) for i in range(self.block_size)]
+        z_fwd_all = torch.stack(z_fwd_list, dim=2).view(batch_size, self.hidden_dim)
         zt_obs = concat(z_fwd_all, post_z_1)
 
         zt_obs_list.append(zt_obs)
@@ -118,14 +118,14 @@ class FullQDisentangledVAE(nn.Module):
 
                 z_fwd_list[fwd_t] = self.z_to_c_fwd_list[fwd_t](post_z_1, z_fwd_list[fwd_t],w=wt[:,fwd_t].view(-1,1))
 
-            z_fwd_all = torch.stack(z_fwd_list, dim=2).mean(dim=2).view(batch_size, self.hidden_dim)  #.mean(dim=2)
+            z_fwd_all = torch.stack(z_fwd_list, dim=2).view(batch_size, self.hidden_dim)  #.mean(dim=2)
             # p(xt|zt)
             zt_obs = concat(z_fwd_all, z_post_sample)
             zt_obs_list.append(zt_obs)
             # update weight, w0<...<wd<=1, d means block_size
             #wt = self.z_w_function(concat(z_fwd_all,z_post_sample))
-            wt = self.z_w_function(z_fwd_all)
-            wt = cumsoftmax(wt)
+            #wt = self.z_w_function(z_fwd_all)
+            #wt = cumsoftmax(wt)
             '''
             wt = torch.log(wt) / self.temperature
             wt = torch.exp(wt)
@@ -256,8 +256,8 @@ class Trainer(object):
 
             # init wt
             wt = torch.ones(len, self.model.block_size).to(self.device)
-            z_fwd_list = [torch.zeros(len, self.model.hidden_dim).to(self.device) for i in range(self.model.block_size)]
-            z_fwd_all = torch.stack(z_fwd_list, dim=2).mean(dim=2).view(len, self.model.hidden_dim)
+            z_fwd_list = [torch.zeros(len, self.model.hidden_dim//self.model.block_size).to(self.device) for i in range(self.model.block_size)]
+            z_fwd_all = torch.stack(z_fwd_list, dim=2).view(len, self.model.hidden_dim)
             zt_obs = concat(z_fwd_all, zt_1)
             zt_dec.append(zt_obs)
             for t in range(1, self.model.frames):
@@ -277,7 +277,7 @@ class Trainer(object):
                                                                     w=wt[:, fwd_t].view(-1, 1))
 
 
-                z_fwd_all = torch.stack(z_fwd_list, dim=2).mean(dim=2).view(len, self.model.hidden_dim)  #.mean(dim=2)
+                z_fwd_all = torch.stack(z_fwd_list, dim=2).view(len, self.model.hidden_dim)  #.mean(dim=2)
                 # update weight, w0<...<wd<=1, d means block_size
 
                 z_prior_fwd = self.model.z_prior_out_list(z_fwd_all)
